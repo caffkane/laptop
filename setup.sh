7b8695dea6853b00bb5e74f87ac2bfc1bdf95a9d
Logan Kane <logan.kane@risepeople.com>
2020-01-09 11:37:06 -0800

    edited setup.sh file to install all needed versions for setup

--- setup.sh
+++ setup.sh
@@ -32,17 +32,41 @@ source ~/.zshrc
 
 # Install ruby, nodejs and python
 asdf plugin-add ruby && asdf plugin-add nodejs && asdf plugin-add python
+
+# Install needed ruby versions
 asdf install ruby 2.6.5 
+asdf install ruby 2.3.0
+asdf install ruby 2.3.8
+
 bash ~/.asdf/plugins/nodejs/bin/import-release-team-keyring
+
+# Install needed nodejs versions
 asdf install nodejs 10.16.3
+asdf install nodejs 6.17.1
+asdf install nodejs 6.14.1
+
 asdf install python 3.8.0
-asdf global ruby 2.6.5
+
+# Global versions
+# You will have to set local versions for the repos
+asdf global ruby 2.3.8
 asdf global nodejs 10.16.3
 asdf global python 3.8.0
 
 source ~/.zshrc
 
 # Install powerline
-pip install --user powerline-status
+pip3 install --user powerline-status
+
+. /usr/local/opt/asdf/asdf.sh
+. /usr/local/opt/asdf/etc/bash_completion.d/asdf.bash
 
 source ~/.zshrc
+
+# Install dev tools
+cd ~/
+curl -o https://download.mozilla.org/?product=firefox-latest-ssl&os=osx&lang=en-CA
+curl -o https://go.microsoft.com/fwlink/?LinkID=620882
+curl -o https://iterm2.com/downloads/stable/iTerm2-3_3_7.zip
+curl -o https://dl.pstmn.io/download/latest/osx
+
